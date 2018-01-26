39bee744be64143eaec4c001d558310697f60839
Frank Ngo <frankngomusic@gmail.com>
2018-01-26 15:15:40 -0800

    add css style and additinal UI changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ function Pizza(size){
 
 Pizza.prototype.sizePrice = function(size) {
   var base = 1
-  if(size === "Small"){
+  if(size === "Small ($6)"){
     base = base * 1;
-  } else if (size === "Medium"){
+  } else if (size === "Medium ($7.50"){
     base = base * 1.25;
-  } else if( size === "Large"){
+  } else if( size === "Large ($9)"){
     base = base * 1.5;
-  } else if( size === "Extra Large"){
+  } else if( size === "Extra Large ($10.50)"){
     base = base * 1.75;
   } else {
     alert("Please select a size");
@@ -27,6 +27,31 @@ Pizza.prototype.meatPrice = function() {
   return total;
 }
 
+Pizza.prototype.veggiePrice = function() {
+  var total = 0;
+  $("input:checkbox[name=veggieToppings]:checked").each(function(i, n) {
+    total += parseFloat($(n).val());
+  })
+  return total;
+}
+
+Pizza.prototype.crustPrice = function() {
+  var total = 0;
+  $("input:checkbox[name=crustToppings]:checked").each(function(i, n) {
+    total += parseFloat($(n).val());
+  })
+  return total;
+}
+
+Pizza.prototype.finishPrice = function() {
+  var total = 0;
+  $("input:checkbox[name=finishToppings]:checked").each(function(i, n) {
+    total += parseFloat($(n).val());
+  })
+  return total;
+}
+
+
 $(document).ready(function() {
   $("#pizza-order").submit(function(event){
     event.preventDefault();
@@ -34,9 +59,12 @@ $(document).ready(function() {
     var size = $("#pizzaSize").val();
     var newOrder = new Pizza(size);
     var meatPrice = newOrder.meatPrice();
-    var pizzaPrice = "$" + (parseFloat(newOrder.sizePrice(size)) + meatPrice);
+    var veggiePrice = newOrder.veggiePrice();
+    var crustPrice = newOrder.crustPrice();
+    var finishPrice = newOrder.finishPrice();
+    var pizzaPrice = "$" + (parseFloat(newOrder.sizePrice(size)) + meatPrice + veggiePrice + crustPrice + finishPrice);
 
-    $("#show-order").show();
+    $("#show-order").fadeIn();
     $(".size").text(newOrder.size);
     $(".pizzaPrice").text(pizzaPrice);
 
